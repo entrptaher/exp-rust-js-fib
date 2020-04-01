f8e05a6d4b908212b1142601c2a0a1c91c0cdfda
Taher <entrptaher@gmail.com>
2020-04-02 00:37:32 +0600

    fix: add parcel plugins

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,15 +1,6 @@
-extern crate wasm_bindgen;
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn foo(x: &str) -> String {
-  if x == "abc" {
-    "yes".to_string()
-  } else {
-    "no".to_string()
-  }
-}
+mod lib;
+use lib::fun;
 
 fn main() {
-    println!("Hello, world!");
+  println!("{}", fun("project"))
 }
