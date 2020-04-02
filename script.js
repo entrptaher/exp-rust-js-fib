718853cece5f2f0022deb40473e5b6217d1fbec9
Taher <entrptaher@gmail.com>
2020-04-02 12:19:09 +0600

    chore: add fun

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // import { foo } from "./rust-app/Cargo.toml";
 
-import { fun } from "./rust-app/Cargo.toml";
-console.log(fun('project'));
+// import { fun } from "./rust-app/Cargo.toml";
+// console.log(fun('project'));
 
-// import instance, {
-//   fib as fibRust
-// } from "./rust-app/src/main.rs";
-// import { fib as fibJs } from "./js-app/fib";
+import instance, {
+  fib as fibRust
+} from "./rust-app/src/main.rs";
+import { fib as fibJs } from "./js-app/fib";
 
-// window.test = () => {
-//   console.time("init");
-//   console.timeEnd("init");
+window.test = () => {
+  console.time("init");
+  console.timeEnd("init");
 
-//   console.time("fibJs");
-//   console.log(fibJs(20));
-//   console.timeEnd("fibJs");
+  console.time("fibJs");
+  console.log(fibJs(20));
+  console.timeEnd("fibJs");
 
-//   console.time("fibRust");
-//   console.log(fibRust(20));
-//   console.timeEnd("fibRust");
-// };
+  console.time("fibRust");
+  console.log(fibRust(20));
+  console.timeEnd("fibRust");
+};
 
-// window.test();
+window.test();
